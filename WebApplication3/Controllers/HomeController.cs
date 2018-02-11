85d09a5cb3f79f544a4fae3c891ee265cb24b07e
Алексей <vorlexx@mail.ru>
2018-02-11 18:15:14 +0300

    Добавьте файлы проекта.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        //ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult GetUsers()
+        {
+            List<ApplicationUser> users = new List<ApplicationUser>();
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                users = db.Users.ToList();
+            }
+            return View(users);
+        }
+        //protected override void Dispose(bool disposing)
+        //{
+        //    db.Dispose();
+        //    base.Dispose(disposing);
+        //}
+    }
+}
